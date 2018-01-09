57bb9a285e684b61a5ee0bb4d310ea46cb7eac48
carlos bollero <carlosmbollero@gmail.com>
2018-01-09 12:16:42 -0300

    added recommendations and comment section to Book Details

--- wbooks/src/App/Main/BookDetail/index.js
+++ wbooks/src/App/Main/BookDetail/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './styles.css'
+import './styles.css';
+import Details from './Details';
+import Recommendations from './Recommendations';
+import CommentsSection from './CommentsSection';
 
 class BookDetail extends React.Component {
   render() {
+    const id = this.props.match.params.number - 1;
     return (
-      <Link to='/' className='back-link' >{"< Volver"}</Link>
+      <div>
+        <Link to='/' className='back-link' >{"< Volver"}</Link>
+        <Details bookInfo={this.props.books[id]}/>
+        <Recommendations books={this.props.books} />
+        <CommentsSection />
+      </div>
     );
   }
 }
